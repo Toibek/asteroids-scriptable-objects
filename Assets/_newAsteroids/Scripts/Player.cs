606c78801476bf20b467403354e48565ac198901
Tobias Ek <Bodentobbe@hotmail.com>
2023-01-12 10:55:45 +0100

    Testing stuff with the input system
    Encountered errors with the saving of input actions, need to move the files to outside the one drive since it's broken

--- Assets/_newAsteroids/Scripts/Player.cs
+++ Assets/_newAsteroids/Scripts/Player.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float thrust;
+    [SerializeField] float torque;
+    [SerializeField] float fireRate;
 
-    // Update is called once per frame
-    void Update()
+    Coroutine thrustRoutine;
+    bool thrusting;
+    Coroutine fireRoutine;
+    bool firing;
+    Coroutine torqueRoutine;
+    float currentTorque;
+
+    Rigidbody2D rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void OnMove(InputValue value)
+    {
+        currentTorque = value.Get<Vector2>().y;
+        if (currentTorque != 0 && torqueRoutine == null)
+            torqueRoutine = StartCoroutine(TorqueEnum());
+    }
+    IEnumerator TorqueEnum()
+    {
+        while (currentTorque != 0)
+        {
+            rb.AddTorque(-currentTorque * torque);
+            yield return new WaitForEndOfFrame();
+        }
+        torqueRoutine = null;
+    }
+    void OnThrust()
+    {
+        thrusting = false;
+        if (thrusting && thrustRoutine == null) thrustRoutine = StartCoroutine(thrustEnum());
+    }
+    IEnumerator thrustEnum()
+    {
+        while (thrusting)
+        {
+            rb.AddForce(transform.up * thrust);
+            yield return new WaitForEndOfFrame();
+        }
+        thrustRoutine = null;
+    }
+    void OnFire(InputValue value)
+    {
+        if (value.isPressed) Debug.Log("Fire pressed");
+        else Debug.Log("Fire not pressed");
+    }
+    IEnumerator fireEnum()
+    {
+        while (firing)
+        {
+            //pew
+            yield return new WaitForSeconds(1 / fireRate);
+        }
+        fireRoutine = null;
     }
 }
